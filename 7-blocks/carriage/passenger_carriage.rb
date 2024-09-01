bc61a42b86a2b4275a62f4d83f6eb085953699d0
a.y.kuznetsov <a.y.kuznetsov@fun-box.ru>
2024-09-01 05:21:14 +0300

    Добавление параметра количества посадочных мест для пассажирского вагона

--- 7-blocks/carriage/passenger_carriage.rb
+++ 7-blocks/carriage/passenger_carriage.rb
@@ -3,7 +3,20 @@
 require_relative 'carriage'
 
 class PassengerCarriage < Carriage
-  def initialize(*args)
-    super
+  attr_reader :seats_number, :occupied_seats
+
+  def initialize(number, seats_number)
+    super(number)
+
+    @seats_number = seats_number
+    @occupied_seats = 0
+  end
+
+  def occupy_seat
+    @occupied_seats += 1 if free_seats.positive?
+  end
+
+  def free_seats
+    @seats_number - @occupied_seats
   end
 end
