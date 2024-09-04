fe114b8c0a662db3f6254f21ea06cb44b2141805
a.y.kuznetsov <a.y.kuznetsov@fun-box.ru>
2024-09-05 01:02:55 +0300

    Разделение валидации маршрута

--- 8-rubocop/route/route.rb
+++ 8-rubocop/route/route.rb
@@ -3,6 +3,15 @@
 require_relative '../utils/instance_counter/instance_counter'
 require_relative '../utils/valid'
 
+VALIDATION_ERROR = {
+  error_empty: 'Название маршрута обязательно',
+  error_invalid_len: 'Длина названия должна быть более двух символов',
+  error_no_departure_station: 'Станция отправления обязательна',
+  error_no_arrival_station: 'Станция прибытия обязательна',
+  error_station_wrong_type: 'В качестве аргументов должны быть переданы объекты типа Station',
+  error_same_stations: 'Станции отправления и прибытия должны быть разными'
+}.freeze
+
 class Route
   include InstanceCounter
   include Valid
@@ -19,30 +28,31 @@ class Route
     register_instance
   end
 
-  # rubocop:disable Metrics/PerceivedComplexity
   def validate!
-    error_empty = 'Название маршрута обязательно'
-    error_invalid_len = 'Длина названия должна быть более двух символов'
-    error_no_departure_station = 'Станция отправления обязательна'
-    error_no_arrival_station = 'Станция прибытия обязательна'
-    error_station_wrong_type = 'В качестве аргументов должны быть переданы объекты типа Station'
-    error_same_stations = 'Станции отправления и прибытия должны быть разными'
+    validate_name!
+    validate_stations!
+    validate_station_types!
+  end
 
+  def validate_name!
+    raise VALIDATION_ERROR[:error_empty] if name.nil?
+
+    raise VALIDATION_ERROR[:error_invalid_len] if name.empty? || name.size < 2
+  end
+
+  def validate_stations!
+    raise VALIDATION_ERROR[:error_no_departure_station] if departure_station.nil?
+
+    raise VALIDATION_ERROR[:error_no_arrival_station] if arrival_station.nil?
+
+    raise VALIDATION_ERROR[:error_same_stations] if departure_station == arrival_station
+  end
+
+  def validate_station_types!
     is_correct_typed = departure_station.instance_of?(Station) && arrival_station.instance_of?(Station)
 
-    raise error_empty if name.nil?
-
-    raise error_invalid_len if name.empty? || name.size < 2
-
-    raise error_no_departure_station if departure_station.nil?
-
-    raise error_no_arrival_station if arrival_station.nil?
-
-    raise error_station_wrong_type unless is_correct_typed
-
-    raise error_same_stations if departure_station == arrival_station
+    raise VALIDATION_ERROR[:error_station_wrong_type] unless is_correct_typed
   end
-  # rubocop:enable Metrics/PerceivedComplexity
 
   def add_intermidiate_station(station)
     @intermidiate_stations << station
