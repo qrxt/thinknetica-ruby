76f87e5b9492a460e12623c0d762c7cbc5e77131
a.y.kuznetsov <a.y.kuznetsov@fun-box.ru>
2024-09-05 03:17:17 +0300

    Разделение методов создания сущностей в текстовом интерфейсе

--- 8-rubocop/railroad/pages/create.rb
+++ 8-rubocop/railroad/pages/create.rb
@@ -31,27 +31,7 @@ module PageCreate
     attempt_counter = 0
 
     begin
-      puts 'Создание поезда'
-
-      puts "Введите тип поезда (#{highlight('passenger')} или #{highlight('cargo')})"
-
-      type = gets.chomp
-
-      return unless %w[passenger cargo].include?(type)
-
-      puts 'Введите номер поезда (строка):'
-
-      number = gets.chomp
-
-      train = type == 'passenger' ? PassengerTrain.new(number) : CargoTrain.new(number)
-
-      train.manufacturer = prompt_for_manufacturer
-
-      @trains << train
-
-      puts "\nСоздан поезд: #{train.info}\n\n"
-
-      @page = 'create'
+      prompt_for_train_creation
     rescue RuntimeError => e
       attempt_counter += 1
 
@@ -59,25 +39,45 @@ module PageCreate
 
       attempt_counter < 3 ? retry : raise
     end
+  end
+
+  def prompt_for_train_creation
+    puts 'Создание поезда'
+
+    type = prompt_for_train_type
+
+    return unless %w[passenger cargo].include?(type)
+
+    number = prompt_for_train_number
+
+    train = type == 'passenger' ? PassengerTrain.new(number) : CargoTrain.new(number)
+
+    train.manufacturer = prompt_for_manufacturer
+
+    @trains << train
+
+    puts "\nСоздан поезд: #{train.info}\n\n"
+
+    @page = 'create'
+  end
+
+  def prompt_for_train_type
+    puts "Введите тип поезда (#{highlight('passenger')} или #{highlight('cargo')})"
+
+    gets.chomp
+  end
+
+  def prompt_for_train_number
+    puts 'Введите номер поезда (строка):'
+
+    gets.chomp
   end
 
   def create_station
     attempt_counter = 0
 
     begin
-      puts 'Создание станции'
-
-      puts 'Введите название станции:'
-
-      name = gets.chomp
-
-      station = Station.new(name)
-
-      @stations << station
-
-      puts "\nСоздана станция: #{station.name}\n\n"
-
-      @page = 'create'
+      prompt_for_station_creation
     rescue RuntimeError => e
       attempt_counter += 1
 
@@ -87,40 +87,27 @@ module PageCreate
     end
   end
 
+  def prompt_for_station_creation
+    puts 'Создание станции'
+
+    puts 'Введите название станции:'
+
+    name = gets.chomp
+
+    station = Station.new(name)
+
+    @stations << station
+
+    puts "\nСоздана станция: #{station.name}\n\n"
+
+    @page = 'create'
+  end
+
   def create_route
     attempt_counter = 0
 
     begin
-      puts 'Создание маршрута'
-
-      if stations.size < 2
-        puts NOT_ENOUGH_STATIONS_ERROR
-
-        @page = 'create'
-        return
-      end
-
-      puts "Сейчас доступны следующие станции: #{@stations.map(&:name).join(', ')}"
-
-      puts 'Введите название начальной станции:'
-
-      departure_station_name = gets.chomp
-
-      puts 'Введите название конечной станции:'
-
-      arrival_station_name = gets.chomp
-
-      departure_station = @stations.find { |station| station.name == departure_station_name }
-      arrival_station = @stations.find { |station| station.name == arrival_station_name }
-
-      route = Route.new("#{departure_station_name}-#{arrival_station_name}", departure_station, arrival_station)
-
-      @routes << route
-
-      puts "\nСоздан маршрут: #{route.name}\n"
-      puts "Промежуточные станции можно добавить через #{highlight('manage')}\n\n"
-
-      @page = 'create'
+      prompt_for_route_creation
     rescue RuntimeError => e
       attempt_counter += 1
 
@@ -128,5 +115,42 @@ module PageCreate
 
       attempt_counter < 3 ? retry : raise
     end
+  end
+
+  def prompt_for_route_creation
+    puts 'Создание маршрута'
+
+    return if check_no_stations
+
+    departure_station = prompt_for_departure_station
+    arrival_station = prompt_for_arrival_station
+
+    route = Route.new("#{departure_station.name}-#{arrival_station.name}", departure_station, arrival_station)
+
+    @routes << route
+
+    puts "\nСоздан маршрут: #{route.name}\n"
+
+    @page = 'create'
+  end
+
+  def prompt_for_departure_station
+    puts 'Введите название начальной станции:'
+
+    prompt_for_station
+  end
+
+  def prompt_for_arrival_station
+    puts 'Введите название конечной станции:'
+
+    prompt_for_station
+  end
+
+  def check_no_stations
+    return if stations.size > 2
+
+    puts NOT_ENOUGH_STATIONS_ERROR
+
+    @page = 'create'
   end
 end
